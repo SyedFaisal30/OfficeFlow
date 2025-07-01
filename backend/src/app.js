9c8b6bfa23a6e3702dce5a7d262ff0ec8900d45b
SyedFaisal30 <sfarz172320@gmail.com>
2025-07-01 20:56:28 +0530

    P2

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import adminRouter from './routes/admin.routes.js';
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://123lms00-5173.inc1.devtunnels.ms/"],
+    origin: ["http://localhost:5173", "https://123lms00-5173.inc1.devtunnels.ms/", "https://officeflows.vercel.app/"],
     credentials: true
 }));
 
